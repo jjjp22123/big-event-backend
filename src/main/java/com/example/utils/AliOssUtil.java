0c26994e9a83c3aabe7aa76a216ebe1ce1b2db4a
jjjp240626 <2379180264@qq.com>
2024-08-11 14:21:49 +0800

    ...

--- src/main/java/com/example/utils/AliOssUtil.java
+++ src/main/java/com/example/utils/AliOssUtil.java
@@ -12,13 +12,13 @@ import java.io.InputStream;
 public class AliOssUtil {
 
     // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-    private static final String ENDPOINT = "https://oss-cn-guangzhou.aliyuncs.com";
+    private static final String ENDPOINT = "";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5tRJgz2hn54nNxa5YiYR";
-    private static final String ACCESS_KEY_SECRET="rpzAAMNDNJ5TcNko1ThmRo9PMzGuk5";
+    private static final String ACCESS_KEY_ID="";
+    private static final String ACCESS_KEY_SECRET="";
     // 填写Bucket名称，例如examplebucket。
-    private static final String BUCKET_NAME = "forgetbucket";
+    private static final String BUCKET_NAME = "";
 
     public static String uploadFile(String objectName, InputStream in) throws Exception {
 
